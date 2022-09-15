2ec1a79b4db94a33f33b06d088b93110809025f9
Kauan Santos de Pontes <kauan.pontes@cartaobrb.com.br>
2022-09-15 11:58:41 -0300

    validations and project done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,30 +3,44 @@ import {getRepos} from '/src/scripts/services/repository.js'
 import {user} from '/src/scripts/objects/user.js'
 import {screen} from '/src/scripts/objects/screen.js'
 
+
 document.getElementById('btn-search').addEventListener('click', ()=> {
     const userName = document.getElementById('input-search').value 
-
+     if (validateEmptyInput(userName)) return
     getUserData(userName)
 })
 
+// Event button "Enter"
 document.getElementById('input-search').addEventListener('keyup', (e)=> {
     const userName = e.target.value
     const key = e.which || e.keyCode
     const isEnterKeyPressed = key === 13
 
     if(isEnterKeyPressed){
+        if (validateEmptyInput(userName)) return
         getUserData(userName)
     }
 })
+
+function validateEmptyInput(userName){
+    if(userName.length === 0 ){
+        alert('Preencha o campo com o nome do usuário do GitHub')
+        return true
+    }
+}
 
 async function getUserData(userName) {
    const userReponse = await getUser(userName)
    const repositoriesResponse = await getRepos(userName)
   
+    if(userReponse.message === 'Not Found'){
+        alert('Este usuário não existe')
+        return
+    }
+
    user.setInfo(userReponse)
    user.setRepositories(repositoriesResponse)
    screen.renderUser(user)
-   
 }
 
 
